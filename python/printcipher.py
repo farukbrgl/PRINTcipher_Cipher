33b6a7dce314e6e3fffec82675072791b6ba297b
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-02-14 17:14:04 +0300

    update printcipher

--- python/printcipher.py
+++ python/printcipher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# execution time : 20.792 s
 """
 PRINTcipher Hafif Blok Şifreleyicisinin Python ile Gerçeklenmesi
 Orijinal makaleye https://link.springer.com/chapter/10.1007/978-3-642-15031-9_2
@@ -8,11 +10,11 @@ adresinden ulaşabilirsiniz.
 
 """
 """
+
 import printcipher_round
 import printcipher_S_layer
 import printcipher_P_layer
 import printcipher_RCi
-
 key = 0xC28895BA327B
 plaintext = "10011001000010001110101010101011100001101011011"
 permkey = 0x69D2CDB6
@@ -31,13 +33,13 @@ i_round = 0
 #     plaintext = printcipher_round.round(plaintext, key, permkey_bin, i_round, RCi_list)
 # print(plaintext)
 
-f = open("random_numbers.txt", "w")
+f = open("random_numbers_printcipher.txt", "w")
 # prng için
-for m in range (10000):
-    for i_round in range (48):
+for m in range(10000):
+    for i_round in range(48):
         # print(plaintext, "before")
-        plaintext = printcipher_round.round(plaintext, key, permkey_bin, i_round, RCi_list)
+        plaintext = printcipher_round.round(
+            plaintext, key, permkey_bin, i_round, RCi_list)
 
-    
-    f.write(plaintext+"\n")
+    f.write(plaintext + "\n")
 f.close()
